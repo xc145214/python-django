c51474a303a4270662431ddc2b5289da47576d25
xc145214 <xc145214@gmail.com>
2015-06-11 12:48:43 +0800

    create project

---
+++ hello/hello/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+#import view
+from views import hello,current_datetime,hours_ahead
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'hello.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    # hello
+    ('^hello/$', hello),
+
+    # time
+    ('^time/$', current_datetime),
+
+    #another url
+    ('^another-time-page/$', current_datetime),
+
+    #
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
+)
